9c57f0a56c1cd393db1ab1ccca3faaf57faac9e3
Emmanuel B <blancocemmanuel@gmail.com>
2025-09-25 06:00:41 +1000

    API post

---
+++ APIpost/test.js
@@ -0,0 +1,21 @@
+console.log = function() {};
+const assert = require('chai').assert;
+const fs = require('fs');
+const Structured = require('structured');
+
+const code = fs.readFileSync('public/main.js', 'utf8');
+
+describe('', function () {
+  it('', function() {
+
+    let structureOne = function() {
+      const shortenUrl = () => {
+          fetch(_).then(_).then()
+        }
+      }
+
+    let isMatchOne = Structured.match(code, structureOne);
+    assert.isOk(isMatchOne, 'Did you chain a second `.then()` after the first?');
+
+  });
+});
